838e9b6380734e9842ba6a2ac158da0637070089
Patryk <krasowski.p@outlook.com>
2023-08-27 20:54:25 +0200

    created responsible moon site

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,36 @@ const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.nav__burger')
 const navCloseBtn = document.querySelector('.nav__burger-close')
 const navbar = document.querySelector('.nav__navbar')
+const overlay = document.querySelector('.overlay')
+
+const showNav = () => {
+	overlay.style.display = 'block'
+	navbar.classList.add('nav__navbar--show')
+	setTimeout(() => {
+		navbar.classList.add('nav__navbar--transform')
+	}, 10)
+	document.body.classList.add('hidden')
+}
+
+const closeNav = () => {
+	overlay.style.display = 'none'
+	navbar.classList.remove('nav__navbar--transform')
+	setTimeout(() => {
+		navbar.classList.remove('nav__navbar--show')
+	}, 300)
+	document.body.classList.remove('hidden')
+}
 
 navBtn.addEventListener('click', () => {
-	navbar.classList.add('nav__navbar--animation')
-	// nav.classList.add('nav--show')
+	const events = ['click', 'touchstart']
+	events.forEach(ev => {
+		window.addEventListener(ev, e => {
+			e.target.classList.contains('overlay') ? closeNav() : ''
+		})
+	})
+
+	showNav()
 })
 navCloseBtn.addEventListener('click', () => {
-	navbar.classList.remove('nav__navbar--animation')
-	// setTimeout(() => {
-	// 	nav.classList.remove('nav--show')
-	// }, 300)
+	closeNav()
 })
